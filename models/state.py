3ac6ec37461783a74fdc48145f5fa5fdb0cb389e
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-09-20 11:15:52 +0200

    Create state.py

---
+++ models/state.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''
+  State class
+'''
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+import os
+import models
+
+class State(BaseModel, Base):
+    __tablename__ = 'states'
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", passive_deletes=True, backref="state")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """
+            cities property
+            """
+            city_list = []
+            for key, val in models.storage.all().items():
+                try:
+                    if val.state_id == self.id:
+                        city_list.append(val)
+                except AttributeError:
+                    pass
+            return city_list

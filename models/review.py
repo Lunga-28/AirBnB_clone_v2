6e71d5b194daa42f3a9a51ae00ae5e8d51d925c6
Lunga-28 <124337357+Lunga-28@users.noreply.github.com>
2023-09-20 11:14:33 +0200

    Create review.py

---
+++ models/review.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column, ForeignKey
+import os
+
+class Review(BaseModel, Base):
+
+    __tablename__ = "reviews"
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""

8cf3b99234a5cd492e3ebdce5eb9602dbb0ba2bd
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-09-23 17:52:22 +0200

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,6 @@
+-- creating a database and User
+-- Grants user select priv for performance schema
+-- Should not fail if database exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';

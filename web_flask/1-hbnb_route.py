3774caaa0af097bba8e77cc2965aaf05ea3b7a65
Lunga-28 <124337357+Lunga-28@users.noreply.github.com>
2023-10-25 20:51:11 +0200

    Create 1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+intialize flask web app to listen on 0.0.0.0:5000
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_hello():
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

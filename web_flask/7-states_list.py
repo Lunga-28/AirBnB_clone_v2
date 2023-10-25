b5d8eb1e594bb7432bc5f672762f9a33bcefd964
Lunga-28 <124337357+Lunga-28@users.noreply.github.com>
2023-10-25 20:55:16 +0200

    Create 7-states_list.py

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+intialize flask web app to listen on 0.0.0.0:5000
+"""
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+from operator import attrgetter
+
+app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """retrieve states from database and render templates based on results"""
+    states = []
+    all_states = storage.all(State)
+
+    for val in all_states.values():
+        states.append(val)
+
+    states = sorted(states, key=attrgetter("name"))
+    return render_template('7-states_list.html', states=states)
+
+@app.teardown_appcontext
+def teardown(self):
+    """closes the current session after each request"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
